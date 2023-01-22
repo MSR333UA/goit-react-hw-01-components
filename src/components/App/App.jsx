f4cba911dce797a688ea0d5b53539fb586ede624
MSR333UA <matviivserhii@gmail.com>
2023-01-22 19:46:22 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 // import './App.css'
 
-import { Profile } from 'components/Profile/Profile.jsx';
+import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics.jsx';
 import { FriendList } from 'components/FriendList/FriendList.jsx';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory.jsx';
